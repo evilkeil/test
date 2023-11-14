90e3f8d76de2e7ba778d770cd2e7f550e6d0304f
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-11-14 19:34:53 +0900

    test

--- main6137cc1f73d43f285221.js
+++ main6137cc1f73d43f285221.js
@@ -233,7 +233,8 @@ tl2
   tl3.to(".service-dot", {scale:0.5})
   .to(".service-title", {x:"-50vw", xPercent:-50}, 0)
   .to(".service-dot", {scale:1})
-  .to(".service-main", {y:"-33%"})
+  // .to(".service-main", {y:"-33%"})
+  .to(".service-main", {y:"-110px"})
   .set(".service-main", {backgroundColor:"transparent"})
   .set("#service", {backgroundColor:"#F0F5F5"})
 
@@ -261,4 +262,4 @@ tl2
 
 /******/ })()
 ;
-//# sourceMappingURL=main17df2cb7e849400af348.js.map
+//# sourceMappingURL=main6137cc1f73d43f285221.js.map
